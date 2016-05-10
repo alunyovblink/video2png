2763e9f7f421a67a18652e369e603b23c5ff667a
Alexandr (Sasha) Lunyov <alexandr.lunyov@ffwagency.com>
2016-05-10 19:35:32 +0300

    working on extracting frames from video

--- video_extractor.py
+++ video_extractor.py
@@ -17,12 +17,19 @@ VIDEO_PNG_FRAME_NAME = 'screen_%6d.png'
 class VideoPNGExtractor:
     extension_list = ['mkv', 'mp4', 'webm']
 
+    # video title
+    title = ''
+
     # video url
     download_url = ''
 
     # size
     width = None
     height = None
+
+    # file
+    video_dirpath = ''
+    video_filepath = ''
 
     # auth
     password = None
@@ -40,13 +47,32 @@ class VideoPNGExtractor:
 
         #if (self.width is None and self.height is not None) or (self.width is not None and self.height is None):
         if bool(self.width is not None) ^ bool(self.height is not None):
-          raise VideoPNGExtractorError("Size should be set as pair of Width and Height");
+          raise VideoPNGExtractorError("Size should be set as pair of Width and Height")
 
         # Set password
         if 'password' in args: self.password = args['password']
 
         # Set URL.
         self.download_url = download_url
+
+    def create_work_folder(self, video_title):
+        video_dirpath = os.path.join(VIDEO_PNG_DIR, video_title)
+        if not os.path.exists(video_dirpath):
+            os.makedirs(video_dirpath)
+
+        self.video_dirpath = video_dirpath
+
+    def load(self):
+        path = self.download_url
+
+        if not os.path.isfile(path):
+            raise VideoPNGExtractorError("Selected file is not readable")
+
+        self.video_filepath = path
+        video_filename = os.path.basename(path)
+        video_title = os.path.splitext(video_filename)[0]
+        self.title = video_title
+        self.create_work_folder(video_title)
 
     def download(self):
         # Get video meta info and then download using youtube-dl
@@ -58,6 +84,8 @@ class VideoPNGExtractor:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             meta = ydl.extract_info(self.download_url, download=False)
 
+        self.title = meta['title']
+
         # renaming the file
         # remove special characters from the file name
         video_title = ''.join(c for c in meta['title'] if c.isalnum() or c == '-' or c == '_')
@@ -65,36 +93,60 @@ class VideoPNGExtractor:
         video_filename = video_title + '.' + extension
 
         # create the folder for video
-        video_dirpath = os.path.join(VIDEO_PNG_DIR, video_title);
-        if not os.path.exists(video_dirpath):
-            os.makedirs(video_dirpath);
+        self.create_work_folder(video_title)
 
         # create the full path for video file
-        video_filepath = os.path.join(video_dirpath, video_filename);
+        video_filepath = os.path.join(self.video_dirpath, video_filename)
 
         #
-        cmd = ['youtube-dl', '-k', '-o', video_filepath, self.download_url];
+        cmd = ['youtube-dl', '-k', '-o', video_filepath, self.download_url]
 
-        filter_format = None
+        # if height was set then setting filter format for video
         if self.height is not None:
             filter_format = 'bestvideo[height <=? %(height)i]+bestaudio/best[height <=? %(height)i]' % {
                 "height": self.height}
-            cmd.extend(['-f', filter_format]);
-
-        #cmd.extend(['-k', '-o', video_filename, self.download_url]);
+            cmd.extend(['-f', filter_format])
 
         print("request=%s" % cmd)
 
         # download the video
         subprocess.call(cmd)
 
-        return meta
+        # set file path
+        self.video_filepath = video_filepath
 
-    def validate_video(self):
-        print ""
+    def extract_frames(self):
+        # setting png files names
+        png_filepath = os.path.join(self.video_dirpath, VIDEO_PNG_FRAME_NAME)
 
-    def extract_frames(self, path):
-        print ""
+        # collecting video filtering options
+        video_filter = "select='eq(pict_type,PICT_TYPE_I)'";
+
+        # setting scale options
+        # width scale
+        if self.width is None or self.width <= 0:
+            width = -1
+        else:
+            width = self.width
+
+        # height scale
+        if self.height is None or self.height <= 0:
+            height = -1
+        else:
+            height = self.height
+
+        # adding scale options to video filtering
+        if width <> -1 and height <> -1:
+            scale = "scale='if(gt(a,%(width)i/%(height)i),%(width)i,-1)':'if(gt(a,%(width)i/%(height)i),-1,%(height)i)'" % {"width" : width, "height" : height}
+            video_filter += ",%(scale)s" % {"scale" : scale}
+
+        cmd = ['ffmpeg', '-i', self.video_filepath, '-f', 'image2', '-vf', video_filter, '-vsync', 'vfr', png_filepath]
+
+        print("creating frames...");
+        print(cmd);
+
+        # create iframes
+        subprocess.call(cmd)
 
 class VideoPNGExtractorError(Exception):
     def __init__(self, value):
@@ -106,8 +158,16 @@ def main(argv):
     #extractor = VideoPNGExtractor('https://vimeo.com/164152168', {'height' : 370});
     #extractor = VideoPNGExtractor('https://vimeo.com/164152168', {'width': 320, 'height': 540});
     #extractor = VideoPNGExtractor('https://vimeo.com/164152168', {'width': 320, 'height': 360});
-    extractor = VideoPNGExtractor('https://vimeo.com/164152168', {'width': 320, 'height': 720});
-    extractor.download();
+
+    #extractor = VideoPNGExtractor('https://vimeo.com/164152168', {'width': 320, 'height': 720});
+    #extractor.download()
+
+    extractor = VideoPNGExtractor('temp/test.mp4', {'width': 1024, 'height': 720});
+
+    #extractor = VideoPNGExtractor('temp/test.mp4');
+    extractor.load()
+
+    extractor.extract_frames()
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
