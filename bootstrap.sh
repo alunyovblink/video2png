41d7dc092091810726f7fcdb1a101ff88bf3c10e
Alexandr (Sasha) Lunyov <alexandr.lunyov@ffwagency.com>
2016-05-12 20:48:48 +0300

    added installing gif2apng

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,6 +23,8 @@ sudo apt-get update
 sudo apt-get install python-pip -y
 sudo pip install youtube_dl
 sudo apt-get install pngcrush python-opencv python-numpy python-scipy -y
+sudo apt-get install gif2apng -y
+
 
 #sudo add-apt-repository ppa:mc3man/trusty-media
 #sudo apt-get update
