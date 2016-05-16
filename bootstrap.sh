f008b26cf491e32ad0ca99511c7d72527da40f97
Alexandr (Sasha) Lunyov <alexandr.lunyov@ffwagency.com>
2016-05-16 14:41:58 +0300

    updated bash script for installing soft on virtual machine

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env bash
 
-#sudo apt-get update
-#sudo apt-get install ffmpeg -y
-#sudo apt-get install curl -y
-#sudo curl https://yt-dl.org/latest/youtube-dl -o /usr/local/bin/youtube-dl
-#sudo chmod a+rx /usr/local/bin/youtube-dl
-
-#Install Easy Install. 
-#sudo apt-get install python-setuptools python-dev build-essential
-
-#Install pip. 
-#sudo easy_install pip
-
-#Install virtualenv. 
-#sudo pip install --upgrade virtualenv
-
-# Install ffmpeg
-#sudo apt-get install libav-tools
-
-
 sudo apt-get update
 sudo apt-get install python-pip -y
 sudo pip install youtube_dl
@@ -26,10 +7,7 @@ sudo apt-get install pngcrush python-opencv python-numpy python-scipy -y
 sudo apt-get install gif2apng -y
 
 
-#sudo add-apt-repository ppa:mc3man/trusty-media
-#sudo apt-get update
-#sudo apt-get dist-upgrade -y
-#sudo apt-get install ffmpeg -y
-
-#sudo apt-get install pngcrush python-opencv python-numpy python-scipy
-#sudo apt-get install imagemagick
+sudo add-apt-repository ppa:mc3man/trusty-media
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+sudo apt-get install ffmpeg -y
